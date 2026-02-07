b755cfbc8df0239a888aa504345703b98ba68229
Uday Shinde <ushinde7027@gmail.com>
2026-02-08 00:30:58 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,16 +6,18 @@ const api = axios.create({
 });
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject.error;
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error); // ✅ FIXED
+  }
 );
 
 export default api;
